9c03c0dd9f7a6ceb58b692c03edd20812c4aa6ae
Aaron J Todd <todaaron@amazon.com>
2021-04-27 15:06:17 -0400

    re-enable elasticurl and upgrade crt-java version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,14 +8,7 @@ pluginManagement {
 rootProject.name = "aws-crt-kotlin"
 enableFeaturePreview("GRADLE_METADATA")
 
-/*
-FIXME: we used kotlinx-cli in the elasticurl example and it is now breaking due to bintray shutting down
-disabling because it's breaking downstream consumers of crt-kotlin, re-enable after it's published somewhere else
-or rewrite without the dependency.
-
-see: https://github.com/Kotlin/kotlinx-cli/issues/23
-*/
-//include(":elasticurl")
+include(":elasticurl")
 
 // for local development against crt-java checkout into same parent directory and use composite builds
 // if (file("../aws-crt-java").exists()) {
