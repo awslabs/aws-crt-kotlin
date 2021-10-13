8ff82e41970358e415defc687c887c018df1b72f
Ian Smith Botsford <issmith@amazon.com>
2021-10-13 20:41:59 +0000

    addressing PR feedback

--- src/common/test/aws/sdk/kotlin/crt/http/HttpClientTest.kt
+++ src/common/test/aws/sdk/kotlin/crt/http/HttpClientTest.kt
@@ -164,7 +164,7 @@ private class HttpTestResponseHandler : HttpStreamResponseHandler {
 
     override fun onResponseComplete(stream: HttpStream, errorCode: Int) {
         stream.close()
-        require(streamDone.trySend(errorCode).isSuccess)
+        check(streamDone.trySend(errorCode).isSuccess)
         streamDone.close()
     }
 
