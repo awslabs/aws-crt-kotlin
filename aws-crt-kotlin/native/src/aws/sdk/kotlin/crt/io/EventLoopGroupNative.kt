53af9e2bdf9e6868df292b0dba0f460b9324bcda
Matas Lauzadis <lauzmata@amazon.com>
2024-03-01 13:18:55 -0500

    EventLoopGroup

--- aws-crt-kotlin/native/src/aws/sdk/kotlin/crt/io/EventLoopGroupNative.kt
+++ aws-crt-kotlin/native/src/aws/sdk/kotlin/crt/io/EventLoopGroupNative.kt
@@ -5,8 +5,11 @@
 
 package aws.sdk.kotlin.crt.io
 
-import aws.sdk.kotlin.crt.AsyncShutdown
-import aws.sdk.kotlin.crt.Closeable
+import aws.sdk.kotlin.crt.*
+import aws.sdk.kotlin.crt.Allocator
+import kotlinx.cinterop.*
+import kotlinx.coroutines.channels.Channel
+import libcrt.*
 
 /**
  * Creates a new event loop group for the I/O subsystem to use to run blocking I/O requests
@@ -16,12 +19,42 @@ import aws.sdk.kotlin.crt.Closeable
  * Otherwise, maxThreads will be the number of event loops in the group.
  * @throws [aws.sdk.kotlin.crt.CrtRuntimeException] If the system is unable to allocate space for a native event loop group
  */
-public actual class EventLoopGroup actual constructor(maxThreads: Int) : Closeable, AsyncShutdown {
+@OptIn(ExperimentalForeignApi::class)
+public actual class EventLoopGroup actual constructor(maxThreads: Int) : CrtResource<aws_event_loop_group>(), Closeable, AsyncShutdown {
+    private val elg: CPointer<aws_event_loop_group>
+
+    override val ptr: CPointer<aws_event_loop_group>
+        get() = elg
+
+    private val shutdownCompleteChannel = Channel<Unit>(Channel.RENDEZVOUS)
+    private val channelStableRef = StableRef.create(shutdownCompleteChannel)
+
+    init {
+        val shutdownOpts = cValue<aws_shutdown_callback_options> {
+            shutdown_callback_fn = staticCFunction(::onShutdownComplete)
+            shutdown_callback_user_data = channelStableRef.asCPointer()
+        }
+
+        elg = checkNotNull(aws_event_loop_group_new_default(Allocator.Default, maxThreads.toUShort(), shutdownOpts)) {
+            "aws_event_loop_group_new_default() failed"
+        }
+    }
+
     override suspend fun waitForShutdown() {
-        TODO("Not yet implemented")
+        // FIXME What needs to happen here
     }
 
     override fun close() {
-        TODO("Not yet implemented")
+        println("in close()")
+        aws_event_loop_group_release(elg)
+    }
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private fun onShutdownComplete(userData: COpaquePointer?) {
+    if (userData != null) {
+        val shutdownCompleteChannel = userData.asStableRef<Channel<Unit>>().get()
+        shutdownCompleteChannel.trySend(Unit)
+        shutdownCompleteChannel.close()
     }
 }
