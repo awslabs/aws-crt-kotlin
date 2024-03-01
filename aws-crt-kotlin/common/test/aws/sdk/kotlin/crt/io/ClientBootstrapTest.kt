9578b5b9af47b5d3df523ad9b006436b5e4142b3
Matas Lauzadis <lauzmata@amazon.com>
2024-03-01 13:27:47 -0500

    ClientBootstrap

--- aws-crt-kotlin/common/test/aws/sdk/kotlin/crt/io/ClientBootstrapTest.kt
+++ aws-crt-kotlin/common/test/aws/sdk/kotlin/crt/io/ClientBootstrapTest.kt
@@ -6,14 +6,13 @@
 package aws.sdk.kotlin.crt.io
 
 import aws.sdk.kotlin.crt.CrtTest
-import aws.sdk.kotlin.crt.runSuspendTest
-import kotlin.test.Ignore
+
+import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 
 class ClientBootstrapTest : CrtTest() {
-    @Ignore // FIXME Enable when Kotlin/Native implementation is complete
     @Test
-    fun createDestroy() = runSuspendTest {
+    fun createDestroy() = runTest {
         val elg = EventLoopGroup()
         val hr = HostResolver(elg)
         val bootstrap = ClientBootstrap(elg, hr)
--- aws-crt-kotlin/native/src/aws/sdk/kotlin/crt/io/ClientBootstrapNative.kt
+++ aws-crt-kotlin/native/src/aws/sdk/kotlin/crt/io/ClientBootstrapNative.kt
@@ -5,15 +5,54 @@
 
 package aws.sdk.kotlin.crt.io
 
-import aws.sdk.kotlin.crt.AsyncShutdown
-import aws.sdk.kotlin.crt.Closeable
+import aws.sdk.kotlin.crt.*
+import aws.sdk.kotlin.crt.Allocator
+import kotlinx.cinterop.*
+import kotlinx.coroutines.channels.Channel
+import libcrt.aws_client_bootstrap
+import libcrt.aws_client_bootstrap_new
+import libcrt.aws_client_bootstrap_options
+import libcrt.aws_client_bootstrap_release
 
-public actual class ClientBootstrap actual constructor(elg: EventLoopGroup, hr: HostResolver) : Closeable, AsyncShutdown {
+@OptIn(ExperimentalForeignApi::class)
+public actual class ClientBootstrap actual constructor(
+    elg: EventLoopGroup,
+    hr: HostResolver
+) : CrtResource<aws_client_bootstrap>(), Closeable, AsyncShutdown {
+    private val bootstrap: CPointer<aws_client_bootstrap>
+    private val shutdownCompleteChannel = Channel<Unit>(Channel.RENDEZVOUS)
+    private val channelStableRef = StableRef.create(shutdownCompleteChannel)
+
+    override val ptr: CPointer<aws_client_bootstrap>
+        get() = bootstrap
+
+    init {
+        val opts = cValue<aws_client_bootstrap_options> {
+            event_loop_group = elg.ptr
+            host_resolver = hr.ptr
+            on_shutdown_complete = staticCFunction(::onShutdownComplete)
+            user_data = channelStableRef.asCPointer()
+        }
+
+        bootstrap = checkNotNull(aws_client_bootstrap_new(Allocator.Default, opts)) {
+            "aws_client_bootstrap_new() failed"
+        }
+    }
+
     override suspend fun waitForShutdown() {
-        TODO("Not yet implemented")
+        // FIXME What needs to happen here? Does anything need to happen?
     }
 
     override fun close() {
-        TODO("Not yet implemented")
+        aws_client_bootstrap_release(bootstrap)
+    }
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private fun onShutdownComplete(userData: COpaquePointer?) {
+    if (userData != null) {
+        val shutdownCompleteChannel = userData.asStableRef<Channel<Unit>>().get()
+        shutdownCompleteChannel.trySend(Unit)
+        shutdownCompleteChannel.close()
     }
 }
