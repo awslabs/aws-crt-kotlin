2b765e1117850b40c46b2f748cadaadd8e376b42
Matas Lauzadis <lauzmata@amazon.com>
2024-03-04 09:50:20 -0500

    MutableBuffer

--- aws-crt-kotlin/native/src/aws/sdk/kotlin/crt/io/MutableBufferNative.kt
+++ aws-crt-kotlin/native/src/aws/sdk/kotlin/crt/io/MutableBufferNative.kt
@@ -4,33 +4,65 @@
  */
 package aws.sdk.kotlin.crt.io
 
+import aws.sdk.kotlin.crt.Allocator
+import aws.sdk.kotlin.crt.Closeable
+import kotlinx.cinterop.*
+import libcrt.*
+
 /**
  * Represents a mutable linear range of bytes that can be written to.
  * Instance of this class has no additional state except the bytes themselves.
- *
- * NOTE: Platform implementations should provide direct access to the underlying bytes
  */
-public actual class MutableBuffer {
+@OptIn(ExperimentalForeignApi::class)
+public actual class MutableBuffer(buffer: aws_byte_buf? = null, private val capacity: Int) : Closeable { // TODO implement CrtResource?
+    private val buf = buffer ?: Allocator.Default.alloc<aws_byte_buf>()
+
+    public val bytes: ByteArray
+        get() = buf.buffer!!.readBytes(buf.capacity.toInt())
+
+    init {
+        if (buffer == null) {
+            aws_byte_buf_init(buf = buf.ptr, allocator = Allocator.Default.allocator, capacity = capacity.toULong())
+        }
+    }
+
     /**
      * The amount of remaining write capacity before the buffer is full
      */
     public actual val writeRemaining: Int
-        get() = TODO("Not yet implemented")
+        get() = buf.capacity.toInt() - buf.len.toInt()
 
     /**
      * Write as much of [length] bytes from [src] as possible starting at [offset].
      * The number of bytes written is returned which may be less than [length]
      */
     public actual fun write(src: ByteArray, offset: Int, length: Int): Int {
-        TODO("Not yet implemented")
+        src.usePinned { pinnedSrc ->
+            val offsetPinnedSrc = pinnedSrc.addressOf(offset).reinterpret<UByteVar>()
+            if (aws_byte_buf_write(buf.ptr, offsetPinnedSrc, length.toULong())) {
+                return length
+            } else {
+                return 0
+            }
+        }
+    }
+
+    public override fun close() {
+        aws_byte_buf_clean_up(buf.ptr)
     }
 
     public actual companion object {
         /**
          * Create a buffer instance backed by [src]
          */
-        public actual fun of(src: ByteArray): MutableBuffer {
-            TODO("Not yet implemented")
+        public actual fun of(src: ByteArray): MutableBuffer = src.usePinned { pinnedSrc ->
+            val tempBuf: CValue<aws_byte_buf> = aws_byte_buf_from_array(pinnedSrc.addressOf(0), src.size.toULong())
+
+            val buf = Allocator.Default.alloc<aws_byte_buf>()
+            // initialize the buf->buffer
+            aws_byte_buf_init_copy(dest = buf.ptr, allocator = Allocator.Default.allocator, src = tempBuf)
+
+            MutableBuffer(buf, buf.capacity.toInt())
         }
     }
 }
---
+++ aws-crt-kotlin/native/test/aws/sdk/kotlin/crt/io/MutableBufferTest.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package aws.sdk.kotlin.crt.io
+
+import aws.sdk.kotlin.crt.CrtTest
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+
+class MutableBufferTest : CrtTest() {
+    @Test
+    fun testSimpleWrite() {
+        val capacity = 10
+        val buffer = MutableBuffer(capacity = capacity)
+        assertEquals(capacity, buffer.writeRemaining)
+
+        val data = "Hello!"
+        buffer.write(data.encodeToByteArray())
+        assertEquals(capacity - data.length, buffer.writeRemaining)
+
+
+        assertContentEquals(data.encodeToByteArray(), buffer.bytes.copyOfRange(0, data.length))
+    }
+
+    @Test
+    fun testWriteToFullBuffer() {
+        val str = "Hello!"
+        val bytes = str.encodeToByteArray()
+        val buffer = MutableBuffer.of(bytes) // creates a full buffer
+
+        assertEquals(0, buffer.writeRemaining)
+
+        // since it's full, should write 0 bytes
+        assertEquals(0, buffer.write(bytes))
+    }
+}
