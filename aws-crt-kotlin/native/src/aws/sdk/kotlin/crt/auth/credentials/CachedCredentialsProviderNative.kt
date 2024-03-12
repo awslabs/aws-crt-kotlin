6f28b9b51e295192f959980d63b414f4b26adec0
Matas Lauzadis <lauzmata@amazon.com>
2024-03-12 09:48:25 -0400

    building

--- aws-crt-kotlin/native/src/aws/sdk/kotlin/crt/auth/credentials/CachedCredentialsProviderNative.kt
+++ aws-crt-kotlin/native/src/aws/sdk/kotlin/crt/auth/credentials/CachedCredentialsProviderNative.kt
@@ -16,52 +16,53 @@ import libcrt.*
 
 public actual class CachedCredentialsProvider internal actual constructor(builder: CachedCredentialsProviderBuilder) :
     CredentialsProvider {
-    public actual companion object {}\
+    public actual companion object {}
 
     private val shutdownCompleteChannel = Channel<Unit>(1)
     private val channelStableRef = StableRef.create(shutdownCompleteChannel)
 
-    private val provider: aws_credentials_provider
+//    private val provider: aws_credentials_provider
 
     init {
-        provider = memScoped {
-            val opts = cValue<aws_credentials_provider_cached_options> {
-                shutdown_options.apply {
-                    shutdown_callback = staticCFunction(::onShutdownComplete)
-                    shutdown_user_data = channelStableRef.asCPointer()
-                }
-                source = builder.source
-            }
-
-            checkNotNull(aws_credentials_provider_new_cached(Allocator.Default.allocator, opts.ptr)) {
-                "aws_credentials_provider_new_cached"
-            }.pointed
-        }
+//        provider = memScoped {
+//            val opts = cValue<aws_credentials_provider_cached_options> {
+//                shutdown_options.apply {
+//                    shutdown_callback = staticCFunction(::onShutdownComplete)
+//                    shutdown_user_data = channelStableRef.asCPointer()
+//                }
+//                source = builder.source
+//            }
+//
+//            checkNotNull(aws_credentials_provider_new_cached(Allocator.Default.allocator, opts.ptr)) {
+//                "aws_credentials_provider_new_cached"
+//            }.pointed
+//        }
     }
 
 
 
     override suspend fun getCredentials(): Credentials {
-        val credentialsStableRef = StableRef.create(Channel<Credentials>(Channel.RENDEZVOUS))
-
-        awsAssertOpSuccess(
-            s_crt_kotlin_aws_credentials_provider_get_credentials(
-                provider = provider.ptr,
-                callback = staticCFunction(::getCredentialsCallback),
-                user_data = credentialsStableRef.asCPointer()
-            )
-        ) { "aws_credentials_provider_get_credentials()" }
-
-        return credentialsStableRef.get().receive()
+        TODO("Not yet implemented")
+//        val credentialsStableRef = StableRef.create(Channel<Credentials>(Channel.RENDEZVOUS))
+//
+//        awsAssertOpSuccess(
+//            s_crt_kotlin_aws_credentials_provider_get_credentials(
+//                provider = provider.ptr,
+//                callback = staticCFunction(::getCredentialsCallback),
+//                user_data = credentialsStableRef.asCPointer()
+//            )
+//        ) { "aws_credentials_provider_get_credentials()" }
+//
+//        return credentialsStableRef.get().receive()
     }
 
     override fun close() {
-        aws_credentials_provider_release(provider.ptr)
+//        aws_credentials_provider_release(provider.ptr)
     }
 
     override suspend fun waitForShutdown() {
-        shutdownCompleteChannel.receive()
-        shutdownCompleteChannel.close()
-        channelStableRef.dispose()
+//        shutdownCompleteChannel.receive()
+//        shutdownCompleteChannel.close()
+//        channelStableRef.dispose()
     }
 }
