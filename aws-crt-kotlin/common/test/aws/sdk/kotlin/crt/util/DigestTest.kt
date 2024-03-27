e724cfdcb5da3cb08151be5f54d0fa4dbe9ec688
Matas Lauzadis <lauzmata@amazon.com>
2024-03-27 16:00:10 -0400

    Implement SHA-1 and SHA-256

--- aws-crt-kotlin/common/test/aws/sdk/kotlin/crt/util/DigestTest.kt
+++ aws-crt-kotlin/common/test/aws/sdk/kotlin/crt/util/DigestTest.kt
@@ -5,18 +5,27 @@
 
 package aws.sdk.kotlin.crt.util
 
-import kotlin.test.Ignore
+import aws.sdk.kotlin.crt.CrtTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class DigestTest {
-    @Ignore // FIXME Enable when Kotlin/Native implementation is complete
+class DigestTest : CrtTest() {
     @Test
     fun testSha256() {
         val buffer = "I don't know half of you half as well as I should like; and I like less than half of you half as well as you deserve.".encodeToByteArray()
         val raw = Digest.sha256(buffer)
 
         val expected = "96d3145b660d51343b4f49684cbd4a11bf19c11d388bf0f16d396537637e0dd7"
+        val actual = Digest.hex(raw)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testSha1() {
+        val buffer = "I don't know half of you half as well as I should like; and I like less than half of you half as well as you deserve.".encodeToByteArray()
+        val raw = Digest.sha1(buffer)
+
+        val expected = "6d1c8e7fac62963cdf03745cf260f65a165ceb42"
         val actual = Digest.hex(raw)
         assertEquals(expected, actual)
     }
