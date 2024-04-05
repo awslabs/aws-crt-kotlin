a79c167a88e8001e395016840205ecda511a597e
Matas <lauzmata@amazon.com>
2024-04-05 17:18:30 -0400

    feat: add support for native checksums (#100)

--- aws-crt-kotlin/native/src/aws/sdk/kotlin/crt/util/DigestNative.kt
+++ aws-crt-kotlin/native/src/aws/sdk/kotlin/crt/util/DigestNative.kt
@@ -5,6 +5,8 @@
 
 package aws.sdk.kotlin.crt.util
 
+import Sha256
+
 /**
  * Utility object for various hash functions
  */
@@ -12,7 +14,7 @@ public actual object Digest {
     /**
      * Calculate the SHA-256 hash of the input [buffer]
      */
-    public actual fun sha256(buffer: ByteArray): ByteArray {
-        TODO("Not yet implemented")
-    }
+    public actual fun sha256(buffer: ByteArray): ByteArray = Sha256().apply {
+        update(buffer, 0, buffer.size)
+    }.digest()
 }
