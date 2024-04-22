d7f271c39e34484479218105844dab6750d8280a
0marperez <operez899999@gmail.com>
2024-04-22 14:13:58 -0400

    Bug fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,12 +18,13 @@ plugins {
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.13.2"
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.aws.kotlin.repo.tools.kmp)
-    id("artifact-size-metrics") version "1.0" // TODO: CHANGE TO GET IT FROM REMOTE REPO
+    id("artifact-size-metrics") version "0.4.2" // TODO: Use lib.versions.toml
 }
 
 artifactSizeMetrics {
     artifactPrefixes = setOf(":aws-crt-kotlin")
     significantChangeThresholdPercentage = 5.0
+    projectRepositoryName = "aws-crt-kotlin"
 }
 
 allprojects {
