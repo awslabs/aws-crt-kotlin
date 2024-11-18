8a5669ad91029f29165275151a9aa07304eee75d
Matas Lauzadis <lauzmata@amazon.com>
2024-11-18 14:57:48 -0500

    Add KDocs

--- aws-crt-kotlin/native/src/aws/sdk/kotlin/crt/WithCrt.kt
+++ aws-crt-kotlin/native/src/aws/sdk/kotlin/crt/WithCrt.kt
@@ -4,6 +4,9 @@
  */
 package aws.sdk.kotlin.crt
 
+/**
+ * A mixin class used to ensure CRT is initialized before the class is invoked
+ */
 public open class WithCrt() {
     init {
         CRT.initRuntime {  }
