d9e2dbed0fa7d1346a03ff9d1a743811deeb91a2
Matas Lauzadis <lauzmata@amazon.com>
2024-12-16 10:21:41 -0500

    Upgrade kotlinx-binary-compatibility-validator version and refactor to version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ buildscript {
 }
 
 plugins {
-    id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.13.2"
+    alias(libs.plugins.kotlinx.binary.compatibility.validator)
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.aws.kotlin.repo.tools.kmp)
     alias(libs.plugins.aws.kotlin.repo.tools.artifactsizemetrics)
