23eeac0170035f3d1299840e5bbee37bebeb6370
Ian Botsford <83236726+ianbotsf@users.noreply.github.com>
2025-07-29 16:28:55 -0700

    fix: correctly close HTTP-related CRT resources in native sourceset (#169)

--- aws-crt-kotlin/jvm/src/aws/sdk/kotlin/crt/io/HostResolverJVM.kt
+++ aws-crt-kotlin/jvm/src/aws/sdk/kotlin/crt/io/HostResolverJVM.kt
@@ -9,15 +9,22 @@ import aws.sdk.kotlin.crt.AsyncShutdown
 import aws.sdk.kotlin.crt.Closeable
 import software.amazon.awssdk.crt.io.HostResolver as HostResolverJni
 
-public actual class HostResolver actual constructor(elg: EventLoopGroup, maxEntries: Int) :
-    Closeable,
+public actual class HostResolver private constructor(
+    private val elg: EventLoopGroup,
+    private val manageElg: Boolean,
+    maxEntries: Int,
+) : Closeable,
     AsyncShutdown {
     internal val jniHr = HostResolverJni(elg.jniElg, maxEntries)
 
-    public actual constructor(elg: EventLoopGroup) : this(elg, DEFAULT_MAX_ENTRIES)
+    public actual constructor(elg: EventLoopGroup, maxEntries: Int) : this(elg, false, maxEntries)
+    public actual constructor(elg: EventLoopGroup) : this(elg, false, DEFAULT_MAX_ENTRIES)
+    public actual constructor() : this(EventLoopGroup(), true, DEFAULT_MAX_ENTRIES)
 
     actual override fun close() {
         jniHr.close()
+
+        if (manageElg) elg.close()
     }
 
     actual override suspend fun waitForShutdown() {
